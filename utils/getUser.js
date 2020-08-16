44189ad36e15fdf10a0ba1cf487419c42f55c768
Jithin Sathyan <jithinsathyan0709@gmail.com>
2020-08-16 19:29:27 +0530

    Move all utils functions to helper module

--- utils/getUser.js
+++
@@ -1,7 +0,0 @@
-module.exports = getUser;
-
-function getUser(users, firstName, lastName) {
-    const user = users.filter((eachUser) =>
-        eachUser.first_name === firstName.trim() && eachUser.last_name === lastName.trim());
-    return { ...user[0] };
-}
